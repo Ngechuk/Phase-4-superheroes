a94c74edac150c5972f5d7dd4a4b21c8a2c1970e
Kevin <kelvinngechu@gmail.com>
2023-03-27 00:08:07 +0300

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :heroes, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update]
+  resources :hero_powers, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  post '/heroes', to: 'heroes#create'
+
   # root "articles#index"
 end
